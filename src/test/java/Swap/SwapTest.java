d18baa7425e4fa8a12cde9fda425d33d42ba58d6
Luis Garcia <pacalyps23@frontier.com>
2017-05-18 14:50:11 -0400

    finished combiner

--- src/test/java/Swap/SwapTest.java
+++ src/test/java/Swap/SwapTest.java
@@ -6,9 +6,11 @@ import org.junit.Test;
 /**
  * Get the tests passing.
  */
-public class SwapTest {
+public class SwapTest
+{
     @Test
-    public void testSwap() throws Exception {
+    public void testSwap() throws Exception
+    {
         Double[] result = Swap.swap(0,1, 1.5,(double) 2,(double)3);
         Double[] expected = {2.0, 1.5, 3.0};
         Assert.assertArrayEquals(expected, result);
