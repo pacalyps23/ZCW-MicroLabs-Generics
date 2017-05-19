152a3197e81f6f82f90ed3ccb95b8917c4bf0ba5
Luis Garcia <pacalyps23@frontier.com>
2017-05-19 12:06:02 -0400

    finished all labs

--- src/main/java/StackArray/ObjectStack.java
+++ src/main/java/StackArray/ObjectStack.java
@@ -7,10 +7,49 @@ import java.util.Arrays;
  * Remember, you might need to resize the stack in the push method.
  * @param <E>
  */
-public class ObjectStack<E> {
+public class ObjectStack<E>
+{
+    static int count;
     private Object[] elements;
 
-    public ObjectStack() {
+    public ObjectStack()
+    {
+        elements = new Object[10];
+        count = 0;
+    }
 
+    public void push(Object element)
+    {
+        add(element);
+    }
+
+    private void add(Object element)
+    {
+        if(elements.length > count+1)
+        {
+            elements [count] = element;
+        }
+        count++;
+    }
+
+    public boolean isEmpty()
+    {
+        if(count <= 0)
+            return true;
+        else
+            return false;
+    }
+
+    public Object pop()
+    {
+        Object element;
+        if(isEmpty())
+            throw new IndexOutOfBoundsException();
+        else
+        {
+            element = elements [count-1];
+        }
+        count--;
+        return element;
     }
 }
